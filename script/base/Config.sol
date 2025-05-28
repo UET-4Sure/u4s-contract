41ec0ad020a2258f5a60a88834edb5a80e5ca011
Drizzle210 <anhduc5a1tp@gmail.com>
2025-05-28 12:47:28 +0700

    create pool and mint liquidity

--- script/base/Config.sol
+++ script/base/Config.sol
@@ -8,8 +8,11 @@ import {Currency} from "v4-core/src/types/Currency.sol";
 /// @notice Shared configuration between scripts
 contract Config {
     /// @dev populated with default anvil addresses
-    IERC20 constant token0 = IERC20(address(0x0165878A594ca255338adfa4d48449f69242Eb8F));
-    IERC20 constant token1 = IERC20(address(0xa513E6E4b8f2a923D98304ec87F64353C4D5C853));
+    // IERC20 constant token0 = IERC20(address(0x0165878A594ca255338adfa4d48449f69242Eb8F));
+    // IERC20 constant token1 = IERC20(address(0xa513E6E4b8f2a923D98304ec87F64353C4D5C853));
+    // WBTC & WETH Sepolia network
+    IERC20 constant token0 = IERC20(address(0x099b46d437014D6f234169654A73f4FB56faD10A));
+    IERC20 constant token1 = IERC20(address(0x27D3Fd7B857cdc5CCA5C1898C12f09Ea9F8C8D37));
     IHooks constant hookContract = IHooks(address(0x0));
 
     Currency constant currency0 = Currency.wrap(address(token0));
---
+++ script/sh-script/CreatePoolAndMintLiquidity.sh
@@ -0,0 +1,7 @@
+forge script script/01_CreatePoolAndMintLiquidity.s.sol \
+    --rpc-url $RPC_URL \
+    --private-key $PRIVATE_KEY \
+    --broadcast \
+    --verify \
+    --resume \
+    -vvvv
