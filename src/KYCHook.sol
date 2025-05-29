babe9e39598ac355f3cd05c45c267c197f62a90e
Drizzle210 <anhduc5a1tp@gmail.com>
2025-05-29 14:35:28 +0700

    feat: add kyc hook

---
+++ src/KYCHook.sol
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {BaseHook} from "v4-periphery/src/utils/BaseHook.sol";
+
+import {Hooks} from "v4-core/src/libraries/Hooks.sol";
+import {IPoolManager} from "v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "v4-core/src/types/PoolKey.sol";
+import {PoolId, PoolIdLibrary} from "v4-core/src/types/PoolId.sol";
+import {BalanceDelta} from "v4-core/src/types/BalanceDelta.sol";
+import {BeforeSwapDelta, BeforeSwapDeltaLibrary} from "v4-core/src/types/BeforeSwapDelta.sol";
+
+import {IIdentitySBT} from "./interfaces/IIdentitySBT.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * Only KYC'ed people can trade on the V4 hook'ed pool.
+ */
+contract KYCHook is BaseHook, Ownable {
+    using PoolIdLibrary for PoolKey;
+    IIdentitySBT public identitySBT;
+
+
+    constructor(
+        IPoolManager _poolManager,
+        address _identitySBT
+    ) BaseHook(_poolManager) Ownable(msg.sender) {
+        identitySBT = IIdentitySBT(_identitySBT);
+    }
+
+    modifier onlyPermitKYC(address sender) {
+        require(identitySBT.hasToken(sender), "KYCHook: not permit kyc");
+        _;
+    }
+
+    function getHookPermissions() public pure override returns (Hooks.Permissions memory) {
+        return Hooks.Permissions({
+            beforeInitialize: false,
+            afterInitialize: false,
+            beforeAddLiquidity: true,
+            afterAddLiquidity: false,
+            beforeRemoveLiquidity: true,
+            afterRemoveLiquidity: false,
+            beforeSwap: true,
+            afterSwap: false,
+            beforeDonate: false,
+            afterDonate: false,
+            beforeSwapReturnDelta: false,
+            afterSwapReturnDelta: false,
+            afterAddLiquidityReturnDelta: false,
+            afterRemoveLiquidityReturnDelta: false
+        });
+    }
+
+    function _beforeSwap(address sender, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
+        internal
+        onlyPermitKYC(sender)
+        override
+        returns (bytes4, BeforeSwapDelta, uint24)
+    {
+        return (BaseHook.beforeSwap.selector, BeforeSwapDeltaLibrary.ZERO_DELTA, 0); 
+    }
+
+    function _beforeAddLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) internal onlyPermitKYC(sender) override returns (bytes4) {
+        return BaseHook.beforeAddLiquidity.selector;
+    }
+
+    function _beforeRemoveLiquidity(
+        address sender,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) internal onlyPermitKYC(sender) override returns (bytes4) {
+        return BaseHook.beforeRemoveLiquidity.selector;
+    }
+}
---
+++ src/interfaces/IIdentitySBT.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IIdentitySBT {
+    /// @dev Check whether a given address has a valid identity SBT
+    /// @param _addr Address to check for tokens
+    /// @return valid Whether the address has a valid token
+    function hasToken(address _addr) external view returns (bool valid);
+}
