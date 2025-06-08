6efe91a0858d4ddc884dd533d41edab9598381fc
Drizzle210 <anhduc5a1tp@gmail.com>
2025-06-08 10:59:05 +0700

    deploy new contracts

--- script/base/Constants.sol
+++ script/base/Constants.sol
@@ -19,8 +19,6 @@ contract Constants {
     address constant CREATE2_DEPLOYER = address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
     IAllowanceTransfer constant PERMIT2 = IAllowanceTransfer(address(0x000000000022D473030F116dDEE9F6B43aC78BA3));
 
-    uint256 constant CHAINLINK_DECIMALS = 8;
-
     constructor() {
         POOLMANAGER = IPoolManager(block.chainid.getPoolManagerByChainId());
         posm = PositionManager(payable(block.chainid.getPositionManagerByChainId()));
---
+++ script/sh-script/DeployKycContract.sh
@@ -0,0 +1,6 @@
+forge script script/04_KycContract.s.sol \
+    --rpc-url $RPC_URL \
+    --private-key $PRIVATE_KEY \
+    --broadcast \
+    --verify \
+    -vvvv
